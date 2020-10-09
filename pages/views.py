cac30a9611e557dec3d8340d57097023d7d0da23
Hubi <h.cieslewicz2wp.pl>
2020-10-09 16:15:07 +0200

    added basic search functionality

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
 from listings.models import Realtor
 
@@ -8,7 +8,10 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
     
     return render(request, 'pages/index.html', context)
